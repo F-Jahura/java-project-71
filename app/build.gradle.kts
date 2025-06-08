2b07038af227ada648cb7ca8fa432824486e958f
F-Jahura <f_jahura@yahoo.com>
2025-06-08 23:55:31 +0300

    added json files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     id("java")
-    application
+    id("application")
     id("com.github.ben-manes.versions") version "0.52.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "hexlet.code"
@@ -12,18 +13,27 @@ repositories {
 }
 
 application {
-    // Входная точка
     mainClass.set("hexlet.code.App")
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+                "Main-Class" to "hexlet.code.App"
+        )
+    }
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.apache.commons:commons-lang3:3.17.0")
-    implementation ("info.picocli:picocli:4.7.7")
-    annotationProcessor ("info.picocli:picocli-codegen:4.7.7")
+    implementation("info.picocli:picocli:4.7.7")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.7")
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    implementation ("commons-io:commons-io:2.11.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.3")
 }
-
 
 tasks.test {
     useJUnitPlatform()
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,21 +2,60 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.util.Map;
 import java.util.concurrent.Callable;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         System.out.printf("Hello World!\n");
 
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
+
+        new CommandLine(new App()).parseArgs(args);
+
     }
 
+    @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]")
+    String format;
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    File filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    File filepath2;
+
     @Override
-    public Integer call() throws Exception { 
+    public Integer call() throws Exception {
+        ObjectMapper jasonMapper = new ObjectMapper();
+        ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
+
+        Map<String, Object> data1 = jasonMapper.readValue(filepath1, Map.class);
+        Map<String, Object> data2 = jasonMapper.readValue(filepath2, Map.class);
+
+        // Преобразование в YAML
+        String yaml1 = yamlMapper.writeValueAsString(data1);
+        String yaml2 = yamlMapper.writeValueAsString(data2);
+
+        System.out.println(yaml1);
+        System.out.println(yaml2);
+
         return 0;
     }
 }
+
