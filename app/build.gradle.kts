33c821ddec4fe2dc446a67cdef11218ada7e07c8
F-Jahura <f_jahura@yahoo.com>
2025-07-21 21:12:32 +0300

    sonar checque improved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,17 +25,6 @@ tasks.jar {
     }
 }
 
-/*dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("org.apache.commons:commons-lang3:3.17.0")
-    implementation("info.picocli:picocli:4.7.7")
-    annotationProcessor("info.picocli:picocli-codegen:4.7.7")
-    implementation ("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
-    implementation ("commons-io:commons-io:2.14.0") // NOSONAR
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.3")
-}*/
-
 val junitBomVersion = "5.9.1"
 val commonsLangVersion = "3.17.0"
 val picocliVersion = "4.7.7"
@@ -80,13 +69,6 @@ val myCheckstyleTest by tasks.registering(Checkstyle::class) {
 tasks.named("check") {
     dependsOn(myCheckstyleTest)
 }
-
-
-/*tasks.register("taskName") {
-    doLast {
-        println("This is a new custom task!")
-    }
-}*/
 
 tasks.jacocoTestReport {
     reports { xml.required.set(true)
---
+++ app/src/test/java/hexlet/code/FormatterTest.java
@@ -0,0 +1,93 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import hexlet.code.formatters.FormatStyle;
+import hexlet.code.formatters.Json;
+import hexlet.code.formatters.Plain;
+import hexlet.code.formatters.Stylish;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.HashMap;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FormatterTest {
+    private static final FormatStyle STYLISH = new Stylish();
+    private static final FormatStyle PLAIN = new Plain();
+    private static final FormatStyle JSON = new Json();
+    @Test
+    public void testRemovedStylish() throws JsonProcessingException {
+        TreeMap<String, Map<String, Object>> dif = new TreeMap<>();
+        Map<String, Object> details = new HashMap<>();
+        details.put("status", "removed");
+        details.put("value", "SomeValue");
+        dif.put("propertyKey", details);
+
+        String result = STYLISH.format(dif);
+        assertTrue(result.contains("- propertyKey: SomeValue"));
+    }
+
+    @Test
+    public void testAddedStylish() throws JsonProcessingException {
+        TreeMap<String, Map<String, Object>> dif = new TreeMap<>();
+        Map<String, Object> details = new HashMap<>();
+        details.put("status", "added");
+        details.put("value", "SomeValue");
+        dif.put("propertyKey", details);
+
+        String result = STYLISH.format(dif);
+        assertTrue(result.contains("+ propertyKey: SomeValue"));
+    }
+
+    @Test
+    public void testUnknownStatus() {
+        TreeMap<String, Map<String, Object>> dif = new TreeMap<>();
+        Map<String, Object> details = new HashMap<>();
+        details.put("status", "unknown");
+        dif.put("propertyKey", details);
+
+        assertThrows(RuntimeException.class, () -> STYLISH.format(dif), "Error value");
+    }
+
+    @Test
+    public void testRemovedPlain() throws JsonProcessingException {
+        TreeMap<String, Map<String, Object>> dif = new TreeMap<>();
+        Map<String, Object> details = new HashMap<>();
+        details.put("status", "removed");
+        details.put("value", "SomeValue");
+        dif.put("key", details);
+
+        String result = PLAIN.format(dif);
+
+        assertTrue(result.contains("Property 'key' was removed"));
+    }
+
+    @Test
+    public void testAddedPlain() throws JsonProcessingException {
+        TreeMap<String, Map<String, Object>> dif = new TreeMap<>();
+        Map<String, Object> details = new HashMap<>();
+        details.put("status", "added");
+        details.put("value", "SomeValue");
+        dif.put("key", details);
+
+        String result = PLAIN.format(dif);
+
+        assertTrue(result.contains("Property 'key' was added with value: 'SomeValue'"));
+    }
+
+    @Test
+    public void testFormatValidData() throws Exception {
+        TreeMap<String, Map<String, Object>> diff = new TreeMap<>();
+        Map<String, Object> details = Map.of("status", "updated", "oldValue", "old", "newValue", "new");
+        diff.put("someKey", details);
+
+        String result = JSON.format(diff);
+
+        assertTrue(result.contains("\"someKey\""));
+        assertTrue(result.contains("\"status\" : \"updated\""));
+    }
+}
